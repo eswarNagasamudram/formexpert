3267126cfba602d2b18a82817599f5949b6ea08d
eswar-prasanth <prasanthsamudram@gmail.com>
2023-12-25 22:53:10 +0530

    adding the model

--- app.py
+++ app.py
@@ -112,35 +112,35 @@ def main():
             live_video_placeholder = st.video(video, format="video/mp4", start_time=0)
         with st.spinner("Getting feedback for your workout..."):
             st.write("Generating feedback")
-            # generator = FormFeedback()
-            # json_data = generator.get_feedback("recorded_video.mp4")
-            # print("JSON DATA IS ----------\n")
-            # print(json_data)
-            # print("===========================")
+            generator = FormFeedback()
+            json_data = generator.get_feedback("recorded_video.mp4")
+            print("JSON DATA IS ----------\n")
+            print(json_data)
+            print("===========================")
 
-            # cleaned_text = json_data.replace("```json\n", "").replace("```","").replace("```json\n{","").replace("JSON","").replace("\n","").strip()
-            # print("Cleaned text IS ----------")
-            # print(cleaned_text)
-            # print("===========================")
-            # try:
-            #     json_data = json.loads(cleaned_text)
-            #     print(json_data)
-            # except json.JSONDecodeError as e:
-            #     print(f"Error decoding JSON: {e}")
-            # st.title(f"# {json_data['workout']} Workout")
-            # st.subheader(f"Form Rating: {json_data['form_rating']}")
+            cleaned_text = json_data.replace("```json\n", "").replace("```","").replace("```json\n{","").replace("JSON","").replace("\n","").strip()
+            print("Cleaned text IS ----------")
+            print(cleaned_text)
+            print("===========================")
+            try:
+                json_data = json.loads(cleaned_text)
+                print(json_data)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+            st.title(f"# {json_data['workout']} Workout")
+            st.subheader(f"Form Rating: {json_data['form_rating']}")
 
-            # st.header("Mistakes:")
-            # for mistake in json_data['mistakes']:
-            #     st.write(f"- {mistake}")
+            st.header("Mistakes:")
+            for mistake in json_data['mistakes']:
+                st.write(f"- {mistake}")
 
-            # st.header("Improvements:")
-            # for improvement in json_data['improvements']:
-            #     st.write(f"- {improvement}")
+            st.header("Improvements:")
+            for improvement in json_data['improvements']:
+                st.write(f"- {improvement}")
 
-            # st.header("Next Steps:")
-            # for next_step in json_data['next_steps']:
-            #     st.write(f"- {next_step}")
+            st.header("Next Steps:")
+            for next_step in json_data['next_steps']:
+                st.write(f"- {next_step}")
 
 
 def toggleRecordingStatus():
@@ -153,61 +153,7 @@ def toggleRecordingStatus():
     
     
 
-        # if "recording_status" not in st.session_state or st.session_state.recording_status == 0:
-        #     st.session_state.recording_status = 1
-
-        # recording_button.empty()
-        # stop_recording = st.button("Stop Recording")
-        # # Countdown timer
-        # live_video_placeholder = st.empty()
-
-        # for i in range(3, 0, -1):
-        #     live_video_placeholder.markdown(f"<h1 style='text-align: center;'>{i}</h1>", unsafe_allow_html=True)
-        #     time.sleep(1)
-
-
-        # live_video_placeholder.empty()
-        # live_video = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
-        # if live_video.video_transformer :
-            # print("entering this block")
-            # if st.session_state.recording_status == 0 or stop_recording:
-            #     print("Processing this block")
-            #     live_video_placeholder.empty()  # Clear the live video section
-            #     live_video_placeholder.video('recorded_video.mp4')
-
-            #     with st.spinner("Getting feedback for your workout..."):
-            #         generator = FormFeedback()
-            #         json_data = generator.get_feedback("recorded_video.mp4")
-            #         print("JSON DATA IS ----------\n")
-            #         print(json_data)
-            #         print("===========================")
-
-            #         cleaned_text = json_data.replace("```json\n", "").replace("```","").replace("```json\n{","").replace("JSON","").replace("\n","").strip()
-            #         print("Cleaned text IS ----------")
-            #         print(cleaned_text)
-            #         print("===========================")
-            #         try:
-            #             json_data = json.loads(cleaned_text)
-            #             print(json_data)
-            #         except json.JSONDecodeError as e:
-            #             print(f"Error decoding JSON: {e}")
-            #         st.title(f"# {json_data['workout']} Workout")
-            #         st.subheader(f"Form Rating: {json_data['form_rating']}")
-
-            #         st.header("Mistakes:")
-            #         for mistake in json_data['mistakes']:
-            #             st.write(f"- {mistake}")
-
-            #         st.header("Improvements:")
-            #         for improvement in json_data['improvements']:
-            #             st.write(f"- {improvement}")
-
-            #         st.header("Next Steps:")
-            #         for next_step in json_data['next_steps']:
-            #             st.write(f"- {next_step}")
-                
-            #         recording_button.empty()
-            #         start_recording = st.button("Start Recording")
+       
 
 if __name__ == "__main__":
     main()
